88331ecfdab8e190c70166aa8e2fb8b1a16b93b4
Gulshan Vasnani <gulshanslvasnani@gmail.com>
2021-06-16 11:25:05 +0530

    linting of CoffeeTable

--- contracts/CoffeeTable.sol
+++ contracts/CoffeeTable.sol
@@ -5,12 +5,17 @@ pragma solidity 0.6.12;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
-import './libraries/NativeMetaTransaction.sol';
-import './libraries/ContextMixin.sol';
+import "./libraries/NativeMetaTransaction.sol";
+import "./libraries/ContextMixin.sol";
+
 // CoffeeTable is the coolest bar in town. You come in with some CNT, and leave with more! The longer you stay, the more CNT you get.
 //
 // This contract handles swapping to and from xCNT, SwapCafe's staking token.
-contract CoffeeTable is ERC20("CoffeeTable", "xCNT") , ContextMixin , NativeMetaTransaction {
+contract CoffeeTable is
+    ERC20("CoffeeTable", "xCNT"),
+    ContextMixin,
+    NativeMetaTransaction
+{
     using SafeMath for uint256;
     IERC20 public cnt;
 
@@ -28,7 +33,6 @@ contract CoffeeTable is ERC20("CoffeeTable", "xCNT") , ContextMixin , NativeMeta
     {
         return ContextMixin.msgSender();
     }
-    
 
     // Enter the bar. Pay some CNTs. Earn some shares.
     // Locks CNT and mints xCNT
@@ -40,7 +44,7 @@ contract CoffeeTable is ERC20("CoffeeTable", "xCNT") , ContextMixin , NativeMeta
         // If no xCNT exists, mint it 1:1 to the amount put in
         if (totalShares == 0 || totalCNT == 0) {
             _mint(_msgSender(), _amount);
-        } 
+        }
         // Calculate and mint the amount of xCNT the CNT is worth. The ratio will change overtime, as xCNT is burned/minted and CNT deposited + gained from fees / withdrawn.
         else {
             uint256 what = _amount.mul(totalShares).div(totalCNT);
@@ -56,7 +60,8 @@ contract CoffeeTable is ERC20("CoffeeTable", "xCNT") , ContextMixin , NativeMeta
         // Gets the amount of xCNT in existence
         uint256 totalShares = totalSupply();
         // Calculates the amount of CNT the xCNT is worth
-        uint256 what = _share.mul(cnt.balanceOf(address(this))).div(totalShares);
+        uint256 what =
+            _share.mul(cnt.balanceOf(address(this))).div(totalShares);
         _burn(_msgSender(), _share);
         cnt.transfer(_msgSender(), what);
     }
