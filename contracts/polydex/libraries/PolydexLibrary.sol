2f7b47473d798e507ae02aad3154712748efe887
Akshit Choudhary <akshitchoudhary007@gmail.com>
2021-07-24 17:24:25 +0530

    fixed typo

--- contracts/polydex/libraries/PolydexLibrary.sol
+++ contracts/polydex/libraries/PolydexLibrary.sol
@@ -9,7 +9,7 @@ import "./SafeMath.sol";
 library PolydexLibrary {
     using SafeMathPolydex for uint;
 
-    // returns sorted token constructorParams, used to handle return values from pairs sorted in this order
+    // returns sorted token addresses, used to handle return values from pairs sorted in this order
     function sortTokens(address tokenA, address tokenB) internal pure returns (address token0, address token1) {
         require(tokenA != tokenB, 'PolydexLibrary: IDENTICAL_ADDRESSES');
         (token0, token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
