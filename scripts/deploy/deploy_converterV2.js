7128712134df39336d6b81a407c77b5b7b99c921
saurabh-lodha-16 <saurabhlodha221b@gmail.com>
2021-08-05 13:01:18 +0530

    Added deployment script for converterV2

---
+++ scripts/deploy/deploy_converterV2.js
@@ -0,0 +1,51 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+const ConstructorParams = require("../constructorParams.json");
+
+async function main() {
+  const ConverterV2 = await hre.ethers.getContractFactory("ConverterV2");
+  const converterInstance = await ConverterV2.deploy(
+    ConstructorParams.POLYDEX_FACTORY,
+    ConstructorParams.CNT_STAKER,
+    ConstructorParams.CNT_TOKEN,
+    ConstructorParams.L2Burner,
+    ConstructorParams.WMATIC,
+    ConstructorParams.BURN_ALLOCATION,
+    ConstructorParams.STAKERS_ALLOCATION,
+    ConstructorParams.PLATFORM_FEES_ALLOCATION,
+    ConstructorParams.FEE_ADDRESS
+  );
+  await converterInstance.deployed();
+  console.log("ConverterV2 deployed at " + converterInstance.address);
+  await converterInstance.deployTransaction.wait([(confirms = 6)]);
+
+  await hre.run("verify:verify", {
+    address: converterInstance.address,
+    constructorArguments: [
+      ConstructorParams.POLYDEX_FACTORY,
+      ConstructorParams.CNT_STAKER,
+      ConstructorParams.CNT_TOKEN,
+      ConstructorParams.L2Burner,
+      ConstructorParams.WMATIC,
+      ConstructorParams.BURN_ALLOCATION,
+      ConstructorParams.STAKERS_ALLOCATION,
+      ConstructorParams.PLATFORM_FEES_ALLOCATION,
+      ConstructorParams.FEE_ADDRESS
+    ],
+  });
+
+  //change router address here if needed to be.
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
