350ba0ed30837f18722db6744f5efaba0413c9ca
Akshit Choudhary <akshitchoudhary007@gmail.com>
2021-10-29 15:03:16 +0530

    fixed wrong variable name

--- scripts/deploy/deployRewardManagerFactory.js
+++ scripts/deploy/deployRewardManagerFactory.js
@@ -1,17 +1,16 @@
 const hre = require("hardhat");
-const ConstructorParams = require("../constructorParams.json");
 
 async function main() {
   // We get the contract to deploy
-  const RewardManager = await hre.ethers.getContractFactory("RewardManager");
-  const rewardManager = await RewardManager.deploy();
+  const RewardManagerFactory = await hre.ethers.getContractFactory("RewardManagerFactory");
+  const rewardManagerFactory = await RewardManagerFactory.deploy();
 
-  console.log("RewardManager deployed at:", rewardManager.address);
+  console.log("RewardManagerFactory deployed at:", rewardManagerFactory.address);
 
-  await rewardManager.deployTransaction.wait([(confirms = 6)]);
+  await rewardManagerFactory.deployTransaction.wait([(confirms = 6)]);
 
   await hre.run("verify:verify", {
-    address: rewardManager.address
+    address: rewardManagerFactory.address
   });
 }
 
