53140abd657e563f2fc1b8683e16793f574ffc36
Akshit Choudhary <akshitchoudhary007@gmail.com>
2021-11-19 19:49:57 +0530

    updated polygon mainnet deployment gas fees

--- hardhat.config.js
+++ hardhat.config.js
@@ -26,7 +26,7 @@ module.exports = {
     matic: {
       url: 'https://polygon-rpc.com',
       accounts: [`0x${PRIVATE_KEY}`],
-      gasPrice: 30000000000, //30 gwei
+      gasPrice: 40000000000, //30 gwei
     },
     mumbai: {
       url: `https://matic-mumbai.chainstacklabs.com`,
