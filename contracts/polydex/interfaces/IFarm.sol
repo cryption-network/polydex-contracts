0ea008fb60a91b3cca2675a8a5516e5f66548c4d
Saurabh Lodha <saurabhlodha221b@gmail.com>
2022-04-27 13:14:07 +0530

    Updated interfaces and added test cases for quickswap migrator

--- contracts/polydex/interfaces/IFarm.sol
+++ contracts/polydex/interfaces/IFarm.sol
@@ -2,5 +2,17 @@
 pragma solidity >=0.6.0 <0.8.0;
 
 interface IFarm {
-    function depositFor(uint256 pid, uint256 amount, address to) external;
+    function withdrawFor(
+        uint256 _pid,
+        uint256 _amount,
+        address _user
+    ) external;
+
+    function depositFor(
+        uint256 _pid,
+        uint256 _amount,
+        address _user
+    ) external;
+
+    function poolLength() external view returns (uint256);
 }
