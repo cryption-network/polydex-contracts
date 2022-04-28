236b8267ab50a0bbc0ba86ddea6da9926fc40afc
Saurabh Lodha <saurabhlodha221b@gmail.com>
2022-04-28 20:56:48 +0530

    Negative test cases and more test cases for migrator

--- test/MIgrator/quickSwapMigrator.test.js
+++ test/MIgrator/quickSwapMigrator.test.js
@@ -3,6 +3,7 @@ const { solidity } = require("ethereum-waffle");
 const chai = require("chai");
 chai.use(solidity);
 const { expect } = chai;
+const { AddressZero } = ethers.constants;
 
 const deployedAddresses = {
   cnt: "0xD1e6354fb05bF72A8909266203dAb80947dcEccF",
@@ -18,6 +19,7 @@ const deployedAddresses = {
   user: "0x10855704d1Dde09d90C0D1afEe4E1e6626e45Bb7",
   polydexFactory: "0x5BdD1CD910e3307582F213b33699e676E61deaD9",
   quickswapFactory: "0x5757371414417b8C6CAad45bAeF941aBc7d3Ab32",
+  wrongLPAddress: "0x67ec655015d1a5F6b61A4560F769C8bb66a8a73F",
 };
 
 const balanceToSetInHex = "0x3635C9ADC5DEA00000";
@@ -81,7 +83,7 @@ describe.only("Quickswap Migrator", function() {
       .updateRewardDistributor(this.quickswapMigrator.address, true);
     await this.rewardManager
       .connect(rewardManagerOwner)
-      .updateWhitelistAddress(2, this.quickswapMigrator.address, true);
+      .updateWhitelistAddress(6, this.quickswapMigrator.address, true);
 
     await this.farm
       .connect(farmOwner)
@@ -111,6 +113,7 @@ describe.only("Quickswap Migrator", function() {
   });
 
   it("should migrate liquidity of CNT-WMATIC farm", async function() {
+    console.log("Initializing pre migration steps");
     let user = deployedAddresses.user;
     await hre.network.provider.request({
       method: "hardhat_impersonateAccount",
@@ -145,7 +148,6 @@ describe.only("Quickswap Migrator", function() {
       .connect(signer)
       .addUserToWhiteList(this.quickswapMigrator.address);
     console.log("Migrator whitelisted by the user");
-
     console.log(
       "WMATIC Balance Before",
       (await this.wmatic.balanceOf(user)).toString()
@@ -154,6 +156,66 @@ describe.only("Quickswap Migrator", function() {
       "CNT Balance Before ",
       (await this.cnt.balanceOf(user)).toString()
     );
+
+    await expect(
+      this.quickswapMigrator
+        .connect(signer)
+        .migrate(oldPid, 0, AddressZero, newPid, deployedAddresses.quickswapLP)
+    ).to.be.revertedWith("QuickSwapMigrator: No zero address");
+    await expect(
+      this.quickswapMigrator
+        .connect(signer)
+        .migrate(oldPid, 0, deployedAddresses.polydexLP, newPid, AddressZero)
+    ).to.be.revertedWith("QuickSwapMigrator: No zero address");
+    await expect(
+      this.quickswapMigrator
+        .connect(signer)
+        .migrate(
+          oldPid,
+          0,
+          deployedAddresses.polydexLP,
+          newPid,
+          deployedAddresses.quickswapLP
+        )
+    ).to.be.revertedWith(
+      "QuickSwapMigrator: LP Amount should be greater than zero"
+    );
+    await expect(
+      this.quickswapMigrator
+        .connect(signer)
+        .migrate(
+          oldPid,
+          0,
+          deployedAddresses.polydexLP,
+          newPid,
+          deployedAddresses.quickswapLP
+        )
+    ).to.be.revertedWith(
+      "QuickSwapMigrator: LP Amount should be greater than zero"
+    );
+    await expect(
+      this.quickswapMigrator
+        .connect(signer)
+        .migrate(
+          1,
+          oldLPTokenAmountInFarm,
+          deployedAddresses.polydexLP,
+          newPid,
+          deployedAddresses.quickswapLP
+        )
+    ).to.be.revertedWith("QuickSwapMigrator: Invalid pids");
+    await expect(
+      this.quickswapMigrator
+        .connect(signer)
+        .migrate(
+          oldPid,
+          oldLPTokenAmountInFarm,
+          deployedAddresses.wrongLPAddress,
+          newPid,
+          deployedAddresses.quickswapLP
+        )
+    ).to.be.revertedWith("QuickSwapMigrator: Invalid LP token addresses");
+
     await this.quickswapMigrator
       .connect(signer)
       .migrate(
@@ -163,6 +225,150 @@ describe.only("Quickswap Migrator", function() {
         newPid,
         deployedAddresses.quickswapLP
       );
+
+    oldLPTokenAmountInFarm = (await this.farm.userInfo(oldPid, signer.address))
+      .amount;
+    console.log(
+      `LP tokens in CNT-WMATIC old farm after migration - ${Number(
+        oldLPTokenAmountInFarm
+      )}`
+    );
+    newLPTokenAmountInFarm = (await this.farm.userInfo(newPid, signer.address))
+      .amount;
+    console.log(
+      `LP tokens in new CNT-WMATIC farm after migration - ${Number(
+        newLPTokenAmountInFarm
+      )}`
+    );
+    userVestedCNT = (
+      await this.rewardManager.userTotalVestingInfo(signer.address)
+    ).totalVested;
+    console.log(`Vested CNT - ${userVestedCNT.toString()}`);
+
+    console.log(
+      "WMATIC Balance After ",
+      (await this.wmatic.balanceOf(user)).toString()
+    );
+    console.log(
+      "CNT Balance After  ",
+      (await this.cnt.balanceOf(user)).toString()
+    );
+  });
+
+  it("should migrate liquidity of CNT-WMATIC farm", async function() {
+    console.log("Initializing pre migration steps");
+    let user = "0x638ecf631cbec94ba8e5134a232fc80d5a0749ac";
+    await hre.network.provider.request({
+      method: "hardhat_impersonateAccount",
+      params: [user],
+    });
+    const signer = await ethers.getSigner(user);
+    await hre.network.provider.send("hardhat_setBalance", [
+      user,
+      balanceToSetInHex,
+    ]);
+    const oldPid = 0;
+    const newPid = 17;
+    const pendingCNT = await this.farm.pendingCNT(oldPid, user);
+    console.log(
+      `Pending CNT for harvest for the user is ${pendingCNT.toString()}`
+    );
+    let oldLPTokenAmountInFarm = (
+      await this.farm.userInfo(oldPid, signer.address)
+    ).amount;
+    console.log(`LP tokens in old CNT-WMATIC farm - ${oldLPTokenAmountInFarm}`);
+    let newLPTokenAmountInFarm = (
+      await this.farm.userInfo(newPid, signer.address)
+    ).amount;
+    console.log(
+      `LP tokens in new CNT-WMATIC farm - ${Number(newLPTokenAmountInFarm)}`
+    );
+    let userVestedCNT = (
+      await this.rewardManager.userTotalVestingInfo(signer.address)
+    ).totalVested;
+    console.log(`Vested CNT - ${userVestedCNT.toString()}`);
+    await this.farm
+      .connect(signer)
+      .addUserToWhiteList(this.quickswapMigrator.address);
+    console.log("Migrator whitelisted by the user");
+    console.log(
+      "WMATIC Balance Before",
+      (await this.wmatic.balanceOf(user)).toString()
+    );
+    console.log(
+      "CNT Balance Before ",
+      (await this.cnt.balanceOf(user)).toString()
+    );
+
+    await expect(
+      this.quickswapMigrator
+        .connect(signer)
+        .migrate(oldPid, 0, AddressZero, newPid, deployedAddresses.quickswapLP)
+    ).to.be.revertedWith("QuickSwapMigrator: No zero address");
+    await expect(
+      this.quickswapMigrator
+        .connect(signer)
+        .migrate(oldPid, 0, deployedAddresses.polydexLP, newPid, AddressZero)
+    ).to.be.revertedWith("QuickSwapMigrator: No zero address");
+    await expect(
+      this.quickswapMigrator
+        .connect(signer)
+        .migrate(
+          oldPid,
+          0,
+          deployedAddresses.polydexLP,
+          newPid,
+          deployedAddresses.quickswapLP
+        )
+    ).to.be.revertedWith(
+      "QuickSwapMigrator: LP Amount should be greater than zero"
+    );
+    await expect(
+      this.quickswapMigrator
+        .connect(signer)
+        .migrate(
+          oldPid,
+          0,
+          deployedAddresses.polydexLP,
+          newPid,
+          deployedAddresses.quickswapLP
+        )
+    ).to.be.revertedWith(
+      "QuickSwapMigrator: LP Amount should be greater than zero"
+    );
+    await expect(
+      this.quickswapMigrator
+        .connect(signer)
+        .migrate(
+          1,
+          oldLPTokenAmountInFarm,
+          deployedAddresses.polydexLP,
+          newPid,
+          deployedAddresses.quickswapLP
+        )
+    ).to.be.revertedWith("QuickSwapMigrator: Invalid pids");
+    await expect(
+      this.quickswapMigrator
+        .connect(signer)
+        .migrate(
+          oldPid,
+          oldLPTokenAmountInFarm,
+          deployedAddresses.wrongLPAddress,
+          newPid,
+          deployedAddresses.quickswapLP
+        )
+    ).to.be.revertedWith("QuickSwapMigrator: Invalid LP token addresses");
+
+    await this.quickswapMigrator
+      .connect(signer)
+      .migrate(
+        oldPid,
+        oldLPTokenAmountInFarm,
+        deployedAddresses.polydexLP,
+        newPid,
+        deployedAddresses.quickswapLP
+      );
+
     oldLPTokenAmountInFarm = (await this.farm.userInfo(oldPid, signer.address))
       .amount;
     console.log(
